7d269b2b066bb1aaadf919a483b2a218c59e16a3
MathiasH <mathiashlm@hotmail.com>
2023-10-22 00:02:47 +0200

    install requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="player-performance-ratings",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        # any dependencies your project has
-    ],
+    install_requires=required,
     author="Mathias Holmstrøm",
     author_email="mathiasholmstom@gmail.com",
     description="Match Predictions based on Player Ratings",
