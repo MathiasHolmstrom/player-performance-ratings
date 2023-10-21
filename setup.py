66f3395d03b937ea42b087d0f88496fe7040765f
MathiasH <mathiashlm@hotmail.com>
2023-10-22 00:21:43 +0200

    setup install requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
+import io
+import os
 from setuptools import setup, find_packages
 
-#with open('requirements.txt') as f:
- #   required = f.read().splitlines()
+
+# with open('requirements.txt') as f:
+#   required = f.read().splitlines()
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+            os.path.join(os.path.dirname(__file__), *paths),
+            encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
 
 setup(
     name="player-performance-ratings",
     version="0.1",
     packages=find_packages(),
-  #  install_requires=,
+    install_requires=read_requirements("requirements.txt"),
     author="Mathias Holmstrøm",
     author_email="mathiasholmstom@gmail.com",
     description="Match Predictions based on Player Ratings",
