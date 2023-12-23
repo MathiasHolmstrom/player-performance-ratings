2bb91c459dc90580d72bc1fd5471bc55e785c7b9
MathiasH <mathiashlm@hotmail.com>
2023-12-23 14:40:47 +0100

    new subsample and notebook

--- player_performance_ratings/examples/nba/data/subsample_nba_data.py
+++ player_performance_ratings/examples/nba/data/subsample_nba_data.py
@@ -5,16 +5,14 @@ gt = pd.read_pickle("game_team.pickle")
 game = pd.read_pickle("game.pickle")
 game = game.rename(columns={'minutes': 'game_minutes'})
 df = df.merge(game[['game_id', 'game_minutes', 'start_date']], on='game_id', how='left')
-df = df.merge(gt[['game_id', 'team_id', 'score', 'score_opponent', 'won']],
-                                on=['game_id', 'team_id'], how='inner')
+df = df.merge(gt[['game_id', 'team_id', "team_id_opponent", 'score', 'score_opponent', 'won']],
+              on=['game_id', 'team_id'], how='inner')
 df.loc[df['won'] == True, 'won'] = 1
 df.loc[df['won'] == False, 'won'] = 0
 
+# randomly select 10% of game_ids
 
-#randomly select 10% of game_ids
-game_ids = df['game_id'].unique().tolist()
-import random
-random.shuffle(game_ids)
-game_ids = game_ids[:int(len(game_ids)*0.1)]
-df = df[df['game_id'].isin(game_ids)]
+df = df[["team_id", "start_date", "game_id", "player_id", "team_id_opponent", "points", "minutes", "won"]]
+df = df[df['start_date'].between('2022-10-17', '2023-02-01')]
+
 df.to_pickle("game_player_subsample.pickle")
