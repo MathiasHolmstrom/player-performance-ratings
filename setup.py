cada3aa72b45daefa2424788609503f77565028d
MathiasH <mathiashlm@hotmail.com>
2024-01-05 19:24:24 +0100

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="player-performance-ratings",
-    version="3.0.0",
+    version="3.1.0",
     packages=find_packages(),
     install_requires=required,
     author="Mathias Holmstrøm",
