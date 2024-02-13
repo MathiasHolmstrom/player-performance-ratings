ee7f4cefe06cf2e32b8c64353f22c16537650f18
MathiasH <mathiashlm@hotmail.com>
2024-02-13 22:39:09 +0100

    rating historical features in enums

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="player-performance-ratings",
-    version="4.03",
+    version="4.0.5",
     packages=find_packages(),
     install_requires=required,
     author="Mathias Holmstrøm",
