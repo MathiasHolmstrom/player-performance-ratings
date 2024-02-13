7d8473f88a3a75dc9b3bcd197e6cc8b480f32d5b
MathiasH <mathiashlm@hotmail.com>
2024-02-13 22:33:51 +0100

    . fix

--- player_performance_ratings/cross_validator/__init__.py
+++ player_performance_ratings/cross_validator/__init__.py
@@ -1,1 +1,2 @@
-from cross_validator import MatchKFoldCrossValidator, MatchCountCrossValidator
+from ._base import CrossValidator
+from .cross_validator import MatchKFoldCrossValidator, MatchCountCrossValidator
