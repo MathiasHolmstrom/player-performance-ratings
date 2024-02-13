96aef19b4b089e6feff5f9bb77d8d264bbac8d94
MathiasH <mathiashlm@hotmail.com>
2024-02-13 22:45:42 +0100

    rating init

--- player_performance_ratings/ratings/__init__.py
+++ player_performance_ratings/ratings/__init__.py
@@ -1,6 +1,10 @@
-from .enums import RatingEstimatorFeatures, RatingHistoricalFeatures,PredictedRatingMethod, InputColumnNames
+from .enums import RatingEstimatorFeatures, RatingHistoricalFeatures, PredictedRatingMethod, InputColumnNames
 from .league_identifier import LeagueIdentifier
 from .match_generator import convert_df_to_matches
 from .time_weight_ratings import BayesianTimeWeightedRating
 from .performances_generator import ColumnWeight, PerformancesGenerator
 from .update_rating_generator import UpdateRatingGenerator
+from .rating_calculators.match_rating_generator import MatchRatingGenerator
+from .rating_calculators.start_rating_generator import StartRatingGenerator
+from .rating_calculators.performance_predictor import RatingDifferencePerformancePredictor, \
+    RatingMeanPerformancePredictor, RatingNonOpponentPerformancePredictor
