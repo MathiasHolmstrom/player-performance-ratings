b7cd653f0e749748685b7ee73a60f0eb893e91b3
MathiasH <mathiashlm@hotmail.com>
2024-02-29 21:00:16 +0100

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="player-performance-ratings",
-    version="4.8.6.1",
+    version="4.8.7",
     packages=find_packages(),
     install_requires=required,
     author="Mathias Holmstrøm",
