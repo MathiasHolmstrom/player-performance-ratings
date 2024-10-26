3703252521bc9756a093d7d06ec81c48fd1f69be
MathiasH <mathiashlm@hotmail.com>
2024-10-26 12:47:36 +0200

    added drop cols to simple transformers

--- player_performance_ratings/predictor_transformer/_simple_transformer.py
+++ player_performance_ratings/predictor_transformer/_simple_transformer.py
@@ -8,6 +8,9 @@ import narwhals as nw
 
 class SimpleTransformer(ABC):
 
+    def __init__(self, drop_cols: Optional[list[str]] = None):
+        self.drop_cols = drop_cols or []
+
     @abstractmethod
     def transform(self, df: FrameT) -> pd.DataFrame:
         pass
@@ -16,7 +19,8 @@ class SimpleTransformer(ABC):
 class AggregatorTransformer(SimpleTransformer):
 
     def __init__(self, columns: list[str],column_to_alias: Optional[dict[str, str]] = None, granularity: Optional[list] = None,
-                 aggregator: Literal['sum', 'mean'] = 'sum'):
+                 aggregator: Literal['sum', 'mean'] = 'sum', drop_cols: Optional[list[str]] = None):
+        super().__init__(drop_cols=drop_cols)
         self.columns = columns
         self.column_to_alias = column_to_alias or {f: f"{f}_{aggregator}" for f in columns}
         self.granularity = granularity
@@ -32,14 +36,14 @@ class AggregatorTransformer(SimpleTransformer):
                     .over(self.granularity)
                     .alias(self.column_to_alias[column])
                     for column in self.columns
-                ).to_pandas()
+                ).drop(self.drop_cols).to_pandas()
             else:
                 return df.with_columns(
                     nw.col(column)
                     .sum()
                     .alias(self.column_to_alias[column])
                     for column in self.columns
-                ).to_pandas()
+                ).drop(self.drop_cols).to_pandas()
 
         elif self.aggregator == 'mean':
             if self.granularity:
@@ -49,24 +53,26 @@ class AggregatorTransformer(SimpleTransformer):
                     .over(self.granularity)
                     .alias(self.column_to_alias[column])
                     for column in self.columns
-                ).to_pandas()
+                ).drop(self.drop_cols).to_pandas()
             else:
                 return df.with_columns(
                     nw.col(column)
                     .mean()
                     .alias(self.column_to_alias[column])
                     for column in self.columns
-                ).to_pandas()
+                ).drop(self.drop_cols).to_pandas()
 
         raise NotImplementedError(f"Aggregator {self.aggregator} not implemented")
 
 
 class NormalizerToColumnTransformer(SimpleTransformer):
 
-    def __init__(self, column: str, granularity: list[str], normalize_to_column: str):
+    def __init__(self, column: str, granularity: list[str], normalize_to_column: str, drop_cols: Optional[list[str]] = None):
+        super().__init__(drop_cols=drop_cols)
         self.column = column
         self.granularity = granularity
         self.normalize_to_column = normalize_to_column
+        self.drop_cols = drop_cols or []
 
     @nw.narwhalify
     def transform(self, df: FrameT) -> pd.DataFrame:
@@ -79,4 +85,4 @@ class NormalizerToColumnTransformer(SimpleTransformer):
         )
         return df.with_columns(
             (nw.col(self.column) / nw.col('__sum_value') * nw.col(self.normalize_to_column)).alias(self.column)
-        ).select(input_cols).to_pandas()
+        ).select(input_cols).drop(self.drop_cols).to_pandas()
