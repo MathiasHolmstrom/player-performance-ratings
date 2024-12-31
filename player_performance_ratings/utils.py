6e6b58df044d004e4033edb84aeb7b644d190033
MathiasH <mathiashlm@hotmail.com>
2024-12-31 12:55:18 +0100

    enables no player_ids for lag-generators in df

--- player_performance_ratings/utils.py
+++ player_performance_ratings/utils.py
@@ -15,60 +15,56 @@ def convert_pandas_to_polars(df: pd.DataFrame) -> pl.DataFrame:
 
 @nw.narwhalify
 def validate_sorting(df: FrameT, column_names: ColumnNames) -> None:
+    sort_cols = [
+        column_names.start_date,
+        column_names.match_id,
+        column_names.team_id,
+        column_names.player_id,
+    ] if column_names.player_id in df.columns else [
+        column_names.start_date,
+        column_names.match_id,
+        column_names.team_id,
+    ]
+    select_cols = [column_names.match_id, column_names.team_id,
+                   column_names.player_id] if column_names.player_id in df.columns else [column_names.match_id,
+                                                                                         column_names.team_id]
+
     df_sorted = df.sort(
-        by=[
-            column_names.start_date,
-            column_names.match_id,
-            column_names.team_id,
-            column_names.player_id,
-        ]
+        by=sort_cols
     )
     if (
-        not df.select(
-            column_names.match_id, column_names.team_id, column_names.player_id
-        )
-        .to_numpy()
-        .tolist()
-        == df_sorted.select(
-            column_names.match_id, column_names.team_id, column_names.player_id
-        )
-        .to_numpy()
-        .tolist()
+            not df.select(select_cols
+            )
+                        .to_numpy()
+                        .tolist()
+                == df_sorted.select(
+                select_cols
+            )
+                        .to_numpy()
+                        .tolist()
     ):
-        for column in [
-            column_names.match_id,
-            column_names.team_id,
-            column_names.player_id,
-        ]:
+        for column in select_cols:
             df = df.with_columns(nw.col(column).cast(nw.String))
 
         df_sorted = df.sort(
-            by=[
-                column_names.start_date,
-                column_names.match_id,
-                column_names.team_id,
-                column_names.player_id,
-            ]
+            by=                sort_cols
+
         )
 
         if (
-            df.select(
-                column_names.match_id, column_names.team_id, column_names.player_id
-            )
-            .to_numpy()
-            .tolist()
-            == df_sorted.select(
-                column_names.match_id, column_names.team_id, column_names.player_id
-            )
-            .to_numpy()
-            .tolist()
+                df.select(
+                    select_cols
+                )
+                        .to_numpy()
+                        .tolist()
+                == df_sorted.select(
+            select_cols
+        )
+                .to_numpy()
+                .tolist()
         ):
             return
-        for column in [
-            column_names.match_id,
-            column_names.team_id,
-            column_names.player_id,
-        ]:
+        for column in select_cols:
             try:
                 df = df.with_columns(nw.col(column).cast(nw.Int64))
 
@@ -76,25 +72,20 @@ def validate_sorting(df: FrameT, column_names: ColumnNames) -> None:
                 pass
 
         df_sorted = df.sort(
-            by=[
-                column_names.start_date,
-                column_names.match_id,
-                column_names.team_id,
-                column_names.player_id,
-            ]
+            by=sort_cols
         )
 
         if (
-            df.select(
-                column_names.match_id, column_names.team_id, column_names.player_id
-            )
-            .to_numpy()
-            .tolist()
-            == df_sorted.select(
-                column_names.match_id, column_names.team_id, column_names.player_id
-            )
-            .to_numpy()
-            .tolist()
+                df.select(
+                    select_cols
+                )
+                        .to_numpy()
+                        .tolist()
+                == df_sorted.select(
+            select_cols
+        )
+                .to_numpy()
+                .tolist()
         ):
             return
 
